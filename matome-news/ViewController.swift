b37118cd753df3e32ad943fd1d03626f8798765a
Hiroshi Kuwabara <hiroshi.kuwabara.81@gmail.com>
2016-06-20 22:13:11 +0900

    init

---
+++ matome-news/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  matome-news
+//
+//  Created by Hiroshi Kuwabara on 6/20/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
