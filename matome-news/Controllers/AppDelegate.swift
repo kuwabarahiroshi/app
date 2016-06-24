8e2e0a1c41d3789f3f97e4ebccfee39bbba7c256
Hiroshi Kuwabara <hiroshi.kuwabara.81@gmail.com>
2016-06-24 23:06:53 +0900

    liftoff

---
+++ matome-news/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  matome-news
+//
+//  Created by Hiroshi Kuwabara on 6/24/16
+//  Copyright (c) 2016 sandbox.io. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
