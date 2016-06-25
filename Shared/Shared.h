304b9286a6585f452ceac1ce37aed79e62c2c833
Hiroshi Kuwabara <hiroshi.kuwabara.81@gmail.com>
2016-06-26 00:24:39 +0900

    added Shared embedded framework

---
+++ Shared/Shared.h
@@ -0,0 +1,19 @@
+//
+//  Shared.h
+//  Shared
+//
+//  Created by Hiroshi Kuwabara on 6/26/16.
+//  Copyright © 2016 sandbox.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shared.
+FOUNDATION_EXPORT double SharedVersionNumber;
+
+//! Project version string for Shared.
+FOUNDATION_EXPORT const unsigned char SharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shared/PublicHeader.h>
+
+
