1a3e625bf84777385c69e475e8d66363d031f962
Hiroshi Kuwabara <hiroshi.kuwabara.81@gmail.com>
2016-06-26 01:44:01 +0900

    ビルドできるとこまで

---
+++ matome-news/Resources/Other-Sources/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  matome-news
+//
+//  Created by Hiroshi Kuwabara on 6/26/16.
+//  Copyright © 2016 sandbox.io. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonCrypto.h>
+
+#endif /* Bridging_Header_h */
